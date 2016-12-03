04668bfb985b1f5a6d1c70af209df117d70183d9
Eric Hebert <ayebear@gmail.com>
2016-12-03 09:39:43 -0500

    Setup rollup.js to build src/ into dist/

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import includePaths from 'rollup-plugin-includepaths'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import json from 'rollup-plugin-json'
+import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+
+let includePathOptions = {
+	include: {},
+	paths: ['src'],
+	external: [],
+	extensions: ['.js']
+}
+
+export default {
+	entry: 'src/index.js',
+	format: 'iife',
+	dest: 'dist/bundle.js',
+	plugins: [
+		includePaths(includePathOptions),
+		nodeResolve({
+			browser: true,
+			preferBuiltins: false,
+			jsnext: true,
+			main: true
+		}),
+		commonjs({
+			ignoreGlobal: true,
+			include: 'node_modules/**'
+		}),
+		json(),
+		babel(),
+		uglify()
+	]
+}
