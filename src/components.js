721cf1982a191e8258ebfa0d1016dac9e9a2a914
Eric Hebert <ayebear@gmail.com>
2016-12-03 14:05:14 -0500

    Added render system and components

--- src/components.js
+++ src/components.js
@@ -1,4 +1,5 @@
 import * as state from 'state'
+import { updateZIndex } from 'utils'
 
 state.world.component('position', function(x, y) {
 	this.x = x || 0
@@ -9,3 +10,42 @@ state.world.component('velocity', function(x, y) {
 	this.x = x || 0
 	this.y = y || 0
 })
+
+// Pixi sprite - used with ZIndex class - instance in state
+state.world.component('sprite', function(container, name, zIndex) {
+	this.s = new PIXI.Sprite(PIXI.utils.TextureCache[name + '.png'])
+	this.s.anchor.x = 0.5
+	this.s.anchor.y = 0.5
+	this.s.zIndex = zIndex || 0
+	container.addChild(this.s)
+	updateZIndex(container)
+
+	this.onRemove = () => {
+		container.removeChild(this.s)
+	}
+
+	// Set texture from name
+	this.set = (name) => {
+		this.s.texture = PIXI.utils.TextureCache[name + '.png']
+	}
+})
+
+state.world.component('mass', function(mass) {
+	this.mass = mass || 0
+})
+
+state.world.component('aabb', function(position, size, offset) {
+	this.rect = new Rectangle(position, size, offset)
+})
+
+state.world.component('lineSegment', function(direction, length) {
+
+})
+
+state.world.component('collisions', function() {
+	this.entities = {}
+})
+
+state.world.component('puck')
+state.world.component('player')
+state.world.component('goal')
---
+++ src/systems/render.js
@@ -0,0 +1,21 @@
+import * as state from 'state'
+
+// Render the stage
+state.world.system(['pixiStage'], class {
+	constructor() {
+		this.renderer = null
+	}
+
+	init() {
+		// Setup pixi renderer and add to page
+		this.renderer = PIXI.autoDetectRenderer(state.gameSize.width, state.gameSize.height, {antialias: false})
+		document.body.appendChild(this.renderer.view)
+
+		// Add entity with pixi stage
+		state.world.entity().set('pixiStage', state.stage)
+	}
+
+	every(pixiStage) {
+		this.renderer.render(pixiStage.root)
+	}
+})
